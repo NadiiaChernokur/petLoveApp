70e00e9a8c91faed35d5839af0e562e98d88ffe5
NadiiaChernokur <chiernokur5@gmail.com>
2024-06-08 12:33:24 +0200

    4

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App.jsx';
+import App from './App';
 import './index.css';
 import 'modern-normalize/modern-normalize.css';
 import { persistor, store } from './redux/store.jsx';
